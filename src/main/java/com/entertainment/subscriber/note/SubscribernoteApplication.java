f76297cb42fb1e200867254c19757ecade77d78d
roticagas <roticagas@gmail.com>
2024-06-06 21:55:11 +0700

    init

---
+++ src/main/java/com/entertainment/subscriber/note/SubscribernoteApplication.java
@@ -0,0 +1,13 @@
+package com.entertainment.subscriber.note;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscribernoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubscribernoteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/entertainment/subscriber/note/SubscribernoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.entertainment.subscriber.note;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscribernoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
