793744bd9dccf707161d5dd52999caaf25b1bd97
roticagas <roticagas@gmail.com>
2024-06-07 15:52:30 +0700

    Add ThreadContextWebFilter and requestId logging
    ThreadContextWebFilter has been implemented to create a unique base64 UUID for each request if it doesn't exist. The requestId is used in logging processes, including alterations made in log4j2.xml and HTTP methods in subscribers.http.

---
+++ src/main/java/com/entertainment/subscriber/note/config/ThreadContextWebFilter.java
@@ -0,0 +1,49 @@
+package com.entertainment.subscriber.note.config;
+
+import org.apache.logging.log4j.ThreadContext;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+
+import java.nio.ByteBuffer;
+import java.util.Base64;
+import java.util.UUID;
+
+/**
+ * A WebFilter implementation that sets the request ID in the ThreadContext.
+ * This class implements the WebFilter interface and is responsible for setting the request ID in the ThreadContext
+ * for each incoming request. If the request does not contain a value for the "X-REQUEST-ID" header, a new unique
+ * request ID will be generated and set in the ThreadContext. The request ID is then available to other components
+ * in the application for logging or any other purpose.
+ */
+@Component
+public class ThreadContextWebFilter implements WebFilter {
+
+    private static String convertUUIDToBase64(UUID uuid) {
+        // Write the UUID to a ByteBuffer
+        ByteBuffer buffer = ByteBuffer.wrap(new byte[16]);
+        buffer.putLong(uuid.getMostSignificantBits());
+        buffer.putLong(uuid.getLeastSignificantBits());
+
+        // Convert to base64 and return
+        return Base64.getUrlEncoder().withoutPadding().encodeToString(buffer.array());
+    }
+
+    /**
+     * Sets the request ID in the ThreadContext for each incoming request.
+     * If the request does not contain a value for the "X-REQUEST-ID" header,
+     * create a new unique Base64 from UUID
+     */
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+        String requestId = exchange.getRequest().getHeaders().getFirst("X-REQUEST-ID");
+        if (requestId == null || requestId.isEmpty()) {
+            requestId = convertUUIDToBase64(UUID.randomUUID());
+        }
+        ThreadContext.put("requestId", requestId);
+        return chain.filter(exchange)
+                .doOnTerminate(ThreadContext::clearMap);
+    }
+}
